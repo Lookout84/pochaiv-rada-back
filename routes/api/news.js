779eccc2e79c167be2e639a79ce45d31801b6ddf
Oleksandr Ivashchuk <67271891+Lookout84@users.noreply.github.com>
2022-03-08 09:21:05 +0200

    save_2

---
+++ routes/api/news.js
@@ -0,0 +1,86 @@
+const express = require('express')
+const router = express.Router()
+const News = require('../../model')
+const {
+    validationCreateNews,
+    validationUpdateNews,
+    validationUpdateStatusNews,
+} = require('./validation')
+
+router.use((req, res, next) => {
+    console.log(req.url)
+    next()
+})
+
+router.get('/', async (req, res, next) => {
+    console.log('Hi')
+    try {
+        const news = await News.getAll()
+        return res.json({ status: 'success', code: 200, data: { news } })
+    } catch (e) {
+        next(e)
+    }
+})
+
+router.get('/:id', async (req, res, next) => {
+    try {
+        const itemNews = await Cats.getById(req.params.id)
+        if (cat) {
+            return res.json({ status: 'success', code: 200, data: { itemNews } })
+        }
+        return res.json({ status: 'error', code: 404, message: 'Not found' })
+    } catch (e) {
+        next(e)
+    }
+})
+
+router.post('/', validationCreateCat, async (req, res, next) => {
+    try {
+        const itemNews = await News.create(req.body)
+        return res.status(201).json({ status: 'success', code: 201, data: { itemNews } })
+    } catch (e) {
+        next(e)
+    }
+})
+
+router.delete('/:id', async (req, res, next) => {
+    try {
+        const itemNews = await News.remove(req.params.id)
+        if (itemNews) {
+            return res.json({ status: 'success', code: 200, data: { itemNews } })
+        }
+        return res.json({ status: 'error', code: 404, message: 'Not found' })
+    } catch (e) {
+        next(e)
+    }
+})
+
+router.put('/:id', validationUpdateCat, async (req, res, next) => {
+    try {
+        const itemNews = await News.update(req.params.id, req.body)
+        if (cat) {
+            return res.json({ status: 'success', code: 200, data: { itemNews } })
+        }
+        return res.json({ status: 'error', code: 404, message: 'Not found' })
+    } catch (e) {
+        next(e)
+    }
+})
+
+router.patch(
+    '/:id/favorited',
+    validationUpdateStatusCat,
+    async (req, res, next) => {
+        try {
+            const itemNews = await News.update(req.params.id, req.body)
+            if (cat) {
+                return res.json({ status: 'success', code: 200, data: { itemNewsNews } })
+            }
+            return res.json({ status: 'error', code: 404, message: 'Not found' })
+        } catch (e) {
+            next(e)
+        }
+    },
+)
+
+module.exports = router
---
+++ routes/api/validation.js
@@ -0,0 +1,47 @@
+const Joi = require('joi')
+
+const schemaCreateNews = Joi.object({
+    author: Joi.string().alphanum().min(3).max(30).required(),
+    name: Joi.string().alphanum().min(3).max(30).required(),
+    text: Joi.string().alphanum().min(3).required(),
+    data: Joi.data(),
+    icon: Joi.string().alphanum().min(3).max(100).required(),
+    isFavorite: Joi.boolean().optional(),
+})
+
+const schemaUpdateNews = Joi.object({
+    author: Joi.string().alphanum().min(3).max(30).required(),
+    name: Joi.string().alphanum().min(3).max(30).required(),
+    text: Joi.string().alphanum().min(3).required(),
+    data: Joi.data(),
+    icon: Joi.string().alphanum().min(3).max(100).required(),
+    isFavorite: Joi.boolean().optional(),
+}).or('author', 'name', 'text', 'data', 'icon', 'isFavorite')
+
+const schemaUpdateStatusNews = Joi.object({
+    isFavorite: Joi.boolean().required(),
+})
+
+const validate = async (schema, obj, next) => {
+    try {
+        await schema.validateAsync(obj)
+        next()
+    } catch (err) {
+        next({
+            status: 400,
+            message: err.message.replace(/"/g, ''),
+        })
+    }
+}
+
+module.exports = {
+    validationCreateNews: (req, res, next) => {
+        return validate(schemaCreateNews, req.body, next)
+    },
+    validationUpdateNews: (req, res, next) => {
+        return validate(schemaUpdateNews, req.body, next)
+    },
+    validationUpdateStatusNews: (req, res, next) => {
+        return validate(schemaUpdateStatusNews, req.body, next)
+    },
+}
