5fca51d46ee6539426993e038129afcdf70846fb
Oleksandr Ivashchuk <67271891+Lookout84@users.noreply.github.com>
2022-06-06 10:14:07 +0300

    Update sessions.js

--- src/repositories/sessions.js
+++ src/repositories/sessions.js
@@ -1,7 +1,6 @@
 const { Session, Author, Variation } = require('../../models')
 const { query } = require('express')
 
-
 const getAllSessions = async () => {
     const result = await Session.find()
     return result
